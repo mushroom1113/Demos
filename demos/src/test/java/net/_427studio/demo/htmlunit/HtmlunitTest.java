54b5452deb5aa64e7a6c46afd05d07f16ff0a158
qidafang <zidafone@163.com>
2015-05-23 00:55:45 +0800

    htmlunit ini

---
+++ demos/src/test/java/net/_427studio/demo/htmlunit/HtmlunitTest.java
@@ -0,0 +1,27 @@
+package net._427studio.demo.htmlunit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+public class HtmlunitTest {
+
+    @Test
+    public void homePage() throws Exception {
+        final WebClient webClient = new WebClient();
+        
+        final HtmlPage page = webClient.getPage("http://page.zidafone.com/");
+        Assert.assertEquals("临时信息发布页", page.getTitleText());
+
+        final String pageAsXml = page.asXml();
+        Assert.assertTrue(pageAsXml.contains("<body>"));
+
+        final String pageAsText = page.asText();
+        Assert.assertTrue(pageAsText.contains("测试临时信息发布"));
+        
+        webClient.close();
+    }
+    
+}
