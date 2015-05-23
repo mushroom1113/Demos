4beb82fac7067ed110719b5613aed80152c03ceb
qidafang <zidafone@163.com>
2015-05-23 16:42:40 +0800

    edit htmlunit test

--- demos/src/test/java/net/_427studio/demo/htmlunit/HtmlunitTest.java
+++ demos/src/test/java/net/_427studio/demo/htmlunit/HtmlunitTest.java
@@ -3,36 +3,25 @@ package net._427studio.demo.htmlunit;
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
-/**
- * htmlunit官网：http://htmlunit.sourceforge.net/
- */
 public class HtmlunitTest {
 
-    @Test
-    public void homePage() throws Exception {
-        final WebClient webClient = new WebClient();
-        
-        final HtmlPage page = webClient.getPage("http://page.zidafone.com/");
+	@Test
+	public void homePage() throws Exception {
+	    final WebClient webClient = new WebClient();
+	    
+        final HtmlPage page = webClient.getPage("http://page.zidafone.com");
         Assert.assertEquals("临时信息发布页", page.getTitleText());
 
         final String pageAsXml = page.asXml();
         Assert.assertTrue(pageAsXml.contains("<body>"));
 
         final String pageAsText = page.asText();
-        Assert.assertTrue(pageAsText.contains("测试临时信息发布"));
+        Assert.assertTrue(pageAsText.contains("临时"));
         
         webClient.close();
-    }
-    
-    @Test
-    public void homePage_Browser() throws Exception {
-        final WebClient webClient = new WebClient(BrowserVersion.CHROME);
-        final HtmlPage page = webClient.getPage("http://page.zidafone.com/");
-        Assert.assertEquals("临时信息发布页", page.getTitleText());
-        webClient.close();
-    }
+	}
+	
 }
